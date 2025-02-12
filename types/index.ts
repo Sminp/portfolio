aff52b546f554f6f707fec4a4e4ca86750de7fa4
Sminp <0102262@gmail.com>
2025-02-12 15:30:16 +0900

    박소민 데이터 json 파일로 쉽게 주고 받을 수 있도록 만듦

--- types/index.ts
+++ types/index.ts
@@ -1,2 +1,51 @@
 export type Theme = "light" | "dark";
 export type Language = "ko" | "en";
+
+export interface General {
+  ko: {
+    greeting: string;
+    projectBtn: string;
+  };
+  en: {
+    greeting: string;
+    projectBtn: string;
+  };
+}
+
+export interface Project {
+  id: number;
+  title: string;
+  description: string;
+  longDescription: string;
+  technologies: string[];
+  image: string;
+  github?: string;
+  demo?: string;
+  date: string;
+}
+
+export interface Projects {
+  ko: Project[];
+  en: Project[];
+}
+
+export interface Profile {
+  contact: {
+    email: string;
+    blog: string;
+    github: string;
+  };
+  skills: string[];
+  ko: {
+    name: string;
+    role: string;
+    about: string;
+    intro: string[];
+  };
+  en: {
+    name: string;
+    role: string;
+    about: string;
+    intro: string[];
+  };
+}

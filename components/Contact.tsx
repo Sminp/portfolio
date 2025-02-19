3ae4076807781856fccc3bb5186e09446b6de459
Sminp <0102262@gmail.com>
2025-02-19 20:10:31 +0900

    박소민 레이아웃 수정

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -5,38 +5,38 @@ export default function Contact() {
   const contact = profileData.contact;
 
   return (
-    <div>
-      <div className="relative overflow-hidden">
-        <div className="h-[230px]">
-          <p className="absolute left-[617px] top-[57px] text-xl font-semibold text-right text-[#4d4f78]">
+    <div className="w-[1440px] h-[334px] relative overflow-hidden bg-[#f2e6ee]">
+      <div className="w-[1040px] h-[334px] absolute left-[200px] top-0 overflow-hidden">
+        <div className="w-[930px] h-[215px]">
+          <p className="w-[97.89px] absolute left-[589.54px] top-[57px] text-xl font-semibold text-right text-[#4d4f78]">
             About me
           </p>
-          <p className="absolute left-[75px] top-[103px] text-xl text-left text-[#4d4f78]">
-            <span className="text-xl text-left text-[#4d4f78]">
+          <p className="w-[358.95px] absolute left-0 top-[105px] text-xl text-left text-[#4d4f78]">
+            <span className="w-[358.95px] text-xl text-left text-[#4d4f78]">
               Tel. : {contact.phone}
             </span>
             <br />
-            <span className="text-xl text-left text-[#4d4f78]">
+            <span className="w-[358.95px] text-xl text-left text-[#4d4f78]">
               <a href={`mailto:${contact.email}`}>Email : {contact.email}</a>
             </span>
             <br />
-            <span className="text-xl text-left text-[#4d4f78]">
+            <span className="w-[358.95px] text-xl text-left text-[#4d4f78]">
               <Link href={contact.github} target="_blank">
                 Github : {contact.github}
               </Link>
             </span>
             <br />
-            <span className="text-xl text-left text-[#4d4f78]">
+            <span className="w-[358.95px] text-xl text-left text-[#4d4f78]">
               <Link href={contact.blog} target="_blank">
                 Blog : {contact.blog}
               </Link>
             </span>
           </p>
-          <p className="absolute left-[854px] top-[57px] text-xl font-semibold text-right text-[#4d4f78]">
+          <p className="w-[82.67px] absolute left-[847.33px] top-[57px] text-xl font-semibold text-right text-[#4d4f78]">
             Projects
           </p>
         </div>
-        <p className="absolute left-[75px] top-[50px] text-[32px] font-semibold text-left text-[#4d4f78]">
+        <p className="absolute left-0 top-[50px] text-[32px] font-semibold text-left text-[#4d4f78]">
           Contact
         </p>
       </div>
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import useLanguageStore from "@/stores/useLanguageStore";
 import profileData from "@/data/profile.json";
 import generalData from "@/data/general.json";
+import Image from "next/image";
+import heroImg from "@/public/hero.png";
 
 export default function Hero() {
   const language = useLanguageStore((state) => state.language);
@@ -11,15 +13,25 @@ export default function Hero() {
   return (
     <div className="flex items-center justify-center min-h-screen px-4">
       <div className="text-center">
-        <p className="text-[64px] text-left">
-          <span className="text-[64px] text-left">{profile.about}</span>
-          <span className="text-[64px] text-left text-[#00033d]"> </span>
-          <br />
-          <span className="text-[64px] text-left text-[#00033d]">
-            {profile.role}입니다.
-          </span>
-        </p>
-        <br />
+        <div className="w-[959.08px] h-[312.08px]">
+          <p className="absolute left-[284px] top-[273px] text-[64px] font-bold text-left">
+            <span className="text-[64px] font-bold text-left bg-gradient-to-r from-[#977DFF] via-[#0033FF] to-[#0600AB] bg-[linear-gradient(-81deg,#977DFF_0%,#0033FF_80%,#0600AB_100%)] bg-clip-text text-transparent">
+              {profile.about}
+            </span>
+            <br />
+            <span className="text-[64px] font-bold text-left text-[#00033d]">
+              {profile.role}
+              {general.greeting}
+            </span>
+          </p>
+          <Image
+            src={heroImg}
+            alt="hero"
+            width={280}
+            height={280}
+            className="absolute left-[970.58px] top-[243.67px] object-cover"
+          />
+        </div>
         <Link
           href="#projects"
           className="bg-blue-600 text-white px-8 py-3 rounded-full hover:bg-blue-700 transition-colors"

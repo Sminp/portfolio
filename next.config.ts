a5dc0e7840d8289231a9104c24f3951db8f05b58
Sminp <0102262@gmail.com>
2025-02-21 15:54:41 +0900

    박소민 빌드 다시 시도

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,9 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: "export", // 정적 사이트로 내보내기 설정
-  basePath: "/portfolio", // GitHub Pages에서는 반드시 필요 (레포지토리명과 동일해야 함!)
-  assetPrefix: "/portfolio/public", // 정적 리소스 경로 설정
+const nextConfig = {
+  output: "export",
+  basePath: "/portfolio", // GitHub Pages에서 올바르게 동작하도록 설정
+  images: {
+    unoptimized: true, // 정적 사이트에서 이미지 최적화 비활성화
+  },
 };
 
 export default nextConfig;

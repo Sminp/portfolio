65bcea614b8ccd14393bd0b9b7085dadc6e16372
Sminp <0102262@gmail.com>
2025-02-21 15:42:08 +0900

    박소민 github actions 설정 준비

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export", // 정적 사이트로 내보내기 설정
+  basePath: "/portfolio", // GitHub Pages에서는 반드시 필요 (레포지토리명과 동일해야 함!)
+  assetPrefix: "/portfolio/public", // 정적 리소스 경로 설정
 };
 
 export default nextConfig;

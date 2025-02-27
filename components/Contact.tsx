b3407b9aa731697fe8cb778d93a0b015ceb4764a
Sminp <0102262@gmail.com>
2025-02-27 11:25:08 +0900

    박소민 줄바꿈 오류 문제 해결 및 contact 박스 크기 수정

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -6,39 +6,31 @@ export default function Contact() {
   const contact = profileData.contact;
 
   return (
-    <div className="w-screen h-[334px] flex items-center justify-center">
-      <div className="w-[1020px] grid grid-cols-2">
-        <div className="flex flex-col">
-          <p className=" text-[32px] font-semibold text-left ">Contact</p>
-          <p className="w-[358.95px]text-xl text-left ">
-            <span className="w-[358.95px] text-xl text-left ">
+    <div className="w-screen h-[534px] flex flex-col justify-end">
+      <div className="w-screen h-[334px] flex items-center justify-center  border-t border-[#4D4F78]">
+        <div className="w-[1240px] h-full grid grid-cols-2 pt-[50px]">
+          <div className="flex flex-col">
+            <p className=" text-[32px] font-semibold text-left pb-[10px]">
+              Contact
+            </p>
+            <p className="text-xl text-left leading-9">
               Tel. : {contact.phone}
-            </span>
-            <br />
-            <span className="w-[358.95px] text-xl text-left">
+              <br />
               <a href={`mailto:${contact.email}`}>Email : {contact.email}</a>
-            </span>
-            <br />
-            <span className="w-[358.95px] text-xl text-left ">
+              <br />
               <Link href={contact.github} target="_blank">
                 Github : {contact.github}
               </Link>
-            </span>
-            <br />
-            <span className="w-[358.95px] text-xl text-left">
+              <br />
               <Link href={contact.blog} target="_blank">
                 Blog : {contact.blog}
               </Link>
-            </span>
-          </p>
-        </div>
-        <div className="flex flex-row justify-around items-start">
-          <p className="w-[97.89px] text-xl font-semibold text-right ">
-            About me
-          </p>
-          <p className="w-[82.67px] text-xl font-semibold text-right ">
-            Projects
-          </p>
+            </p>
+          </div>
+          <div className="flex flex-row justify-around items-start">
+            <p className="text-xl font-semibold text-left ">About me</p>
+            <p className="text-xl font-semibold text-left ">Projects</p>
+          </div>
         </div>
       </div>
     </div>
